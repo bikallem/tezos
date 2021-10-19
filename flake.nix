fa3562d9797acc16d21162a6c0dc6897561d9527
Bikal Lem <gbikal+git@gmail.com>
2021-10-19 10:30:34 +0100

    remove rust packages

--- flake.nix
+++ flake.nix
@@ -4,16 +4,14 @@
   nixConfig.bash-prompt = "[nix-develop]$ ";
 
   inputs = {
-    nixpkgs-21_05.url = "github:nixos/nixpkgs/nixos-21.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs-21_05, nixpkgs-unstable, flake-utils }:
+  outputs = { self, nixpkgs-unstable, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs-unstable.legacyPackages.${system};
-        pkgs-21_05 = nixpkgs-21_05.legacyPackages.${system};
       in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
@@ -25,9 +23,6 @@
             unzip
             wget
             pkg-config
-            pkgs-21_05.rustc
-            pkgs-21_05.cargo
-            pkgs-21_05.rustfmt
             gcc
             gmp
             libev
@@ -37,9 +32,6 @@
             zlib
             opam
           ];
-
-          RUST_SRC_PATH =
-            "${pkgs-21_05.rust.packages.stable.rustPlatform.rustLibSrc}";
         };
       });
 }
